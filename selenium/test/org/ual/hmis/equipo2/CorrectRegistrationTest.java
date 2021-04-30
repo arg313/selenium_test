16d6649e41f9a7126d056efec8daf9f1e2aa4f2d
Alejandro Roca <arg313@inlumine.ual.es>
2021-04-30 11:56:15 +0200

    Cambiando setUp

--- selenium/test/org/ual/hmis/equipo2/CorrectRegistrationTest.java
+++ selenium/test/org/ual/hmis/equipo2/CorrectRegistrationTest.java
@@ -30,30 +30,78 @@ public class CorrectRegistrationTest {
   JavascriptExecutor js;
   @Before
   public void setUp() {
-	  int browser = 0;
-	  Boolean headless = true;
-	  
-	  switch(browser) {
-	  case 0: //firefox
-		  //System.setProperty("webdriver.gecko.driver", "drivers/geckodriver.exe");
-		  FirefoxOptions firefoxOptions = new FirefoxOptions();
-		  if (headless) firefoxOptions.setHeadless(headless);
-		  driver = new FirefoxDriver(firefoxOptions);
-		  driver = new FirefoxDriver(firefoxOptions);
-		  break;
-	  case 1: //chrome
-		  //System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
-		  ChromeOptions chromeOptions = new ChromeOptions();
-		  if (headless) chromeOptions.setHeadless(headless);
-		  chromeOptions.addArguments("window-size=1920,1080");
-		  driver = new ChromeDriver(chromeOptions);
-		  break;
-	  default:
-		  fail("Elige un buscador (0,1)");
-		  break;
-	  }
-	  js = (JavascriptExecutor) driver;
-	  vars = new HashMap<String, Object>();
+	// Using a system property to chose the browser (by jjcanada)
+			// Browser as System.property: "browserWebDriver"
+			// In maven, call: 
+			//    run with firefox: clean test -DbrowserWebDriver=firefox
+			//    run with chrome : clean test -DbrowserWebDriver=chrome 
+
+			// System.setProperty("browserWebDriver", "firefox"); 
+			String browserProperty = ""; 
+			browserProperty= System.getProperty("browserWebDriver");
+			
+			// run headless: clean test -DbrowserWebDriver=firefox -Dheadless=true
+			Boolean headless = false;
+			if (System.getProperty("headless").equals("true")) {
+				headless = true;
+			}
+
+			switch (browserProperty) {
+			case "firefox": 
+				// Firefox 
+				// Descargar geckodriver de https://github.com/mozilla/geckodriver/releases
+				// Descomprimir el archivo geckodriver.exe en la carpeta drivers
+
+				System.setProperty("webdriver.gecko.driver",  "drivers/geckodriver.exe");
+				System.getProperties().list(System.out);
+				FirefoxOptions firefoxOptions = new FirefoxOptions();
+				if (headless) firefoxOptions.setHeadless(headless);
+				driver = new FirefoxDriver(firefoxOptions);
+
+				break;
+			case "chrome":
+				// Chrome
+				// Descargar Chromedriver de https://chromedriver.chromium.org/downloads
+				// Descomprimir el archivo chromedriver.exe en la carpeta drivers
+
+				System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
+				ChromeOptions chromeOptions = new ChromeOptions();
+				if (headless) chromeOptions.setHeadless(headless);
+				chromeOptions.addArguments("window-size=1920,1080");
+				driver = new ChromeDriver(chromeOptions);
+
+				break;
+
+			default:
+				fail("Please select a browser");
+				break;
+			}
+			js = (JavascriptExecutor) driver;
+			vars = new HashMap<String, Object>();
+//	  int browser = 0;
+//	  Boolean headless = true;
+//	  
+//	  switch(browser) {
+//	  case 0: //firefox
+//		  //System.setProperty("webdriver.gecko.driver", "drivers/geckodriver.exe");
+//		  FirefoxOptions firefoxOptions = new FirefoxOptions();
+//		  if (headless) firefoxOptions.setHeadless(headless);
+//		  driver = new FirefoxDriver(firefoxOptions);
+//		  driver = new FirefoxDriver(firefoxOptions);
+//		  break;
+//	  case 1: //chrome
+//		  //System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
+//		  ChromeOptions chromeOptions = new ChromeOptions();
+//		  if (headless) chromeOptions.setHeadless(headless);
+//		  chromeOptions.addArguments("window-size=1920,1080");
+//		  driver = new ChromeDriver(chromeOptions);
+//		  break;
+//	  default:
+//		  fail("Elige un buscador (0,1)");
+//		  break;
+//	  }
+//	  js = (JavascriptExecutor) driver;
+//	  vars = new HashMap<String, Object>();
 	    
 //	  System.setProperty("webdriver.gecko.driver", "drivers/geckodriver.exe");
 //	  System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
